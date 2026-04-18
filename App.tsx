606b0ab47f92dba6b48ce12f125de203fb21f702
Md Muhaiminul <muhai4959@gmail.com>
2026-04-18 20:02:53 +0600

    upgrade to latest version

--- App.tsx
+++ App.tsx
@@ -1,18 +1,16 @@
 import "./global.css"
 import { useEffect } from 'react';
 import { useFonts } from "expo-font";
-import { View } from "react-native";
 import * as SplashScreen from 'expo-splash-screen';
 import { DarkTheme, DefaultTheme, NavigationContainer } from '@react-navigation/native';
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from '@/src/shared/lib/queryClient'
 import { useUniwind } from "uniwind";
 import RootNavigator from "./src/navigation/RootNavigator";
 import { ErrorBoundary } from "./src/shared/components/app/ErrorBoundary";
 import ToastProvider from "./src/shared/components/app/ToastProvider";
-
-const queryClient = new QueryClient();
 
 // Keep the splash screen visible while fetch resources
 SplashScreen.preventAutoHideAsync();
@@ -20,7 +18,7 @@ SplashScreen.preventAutoHideAsync();
 export default function App() {
   const { theme } = useUniwind();
   const [loaded] = useFonts({
-    "Inter": require("@/assets/fonts/Inter-Black.otf"),
+    "Inter-Black": require("@/assets/fonts/Inter-Black.otf"),
   });
 
   useEffect(() => {
@@ -32,27 +30,20 @@ export default function App() {
 
   if (!loaded) {
     return null;
-  };
+  }
 
   return (
-    <View className="flex-1 bg-bg">
-      <ErrorBoundary
-        onError={(error, errorInfo) => {
-          // Log to your error tracking service
-          console.error('App Error:', error);
-        }}
-      >
-        <QueryClientProvider client={queryClient}>
-          <SafeAreaProvider>
-            <GestureHandlerRootView style={{ flex: 1 }}>
-              <NavigationContainer theme={theme === 'dark' ? DarkTheme : DefaultTheme}>
-                <RootNavigator />
-              </NavigationContainer>
+    <GestureHandlerRootView className="flex-1 bg-bg">
+      <SafeAreaProvider>
+        <ErrorBoundary onError={(error) => console.error('App Error:', error)}>
+          <QueryClientProvider client={queryClient}>
+            <NavigationContainer theme={theme === 'dark' ? DarkTheme : DefaultTheme}>
+              <RootNavigator />
               <ToastProvider />
-            </GestureHandlerRootView>
-          </SafeAreaProvider>
-        </QueryClientProvider>
-      </ErrorBoundary>
-    </View>
+            </NavigationContainer>
+          </QueryClientProvider>
+        </ErrorBoundary>
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
   );
 }
